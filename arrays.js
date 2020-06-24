33c5187c1ff665eaf56670303ad3128f2944281b
Oliver Cook <olliewatford@gmail.com>
2020-06-24 10:00:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ var destructivelyAddElementToBeginningOfArray = (array,element) => {
 array.unshift(element);
 return array
 }
+
+var addElementToBeginningOfArray = (array, element) => { 
+return array1 = [...array, element]
+} 
+
+var destructivelyAddElementToEndOfArray = (array,element) => {
+array.push(element);
+return array
+}
