5d080cb711171fc5c614023ecb5f26b520c82664
Oliver Cook <olliewatford@gmail.com>
2020-06-24 09:48:39 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ["snickers", "hundred grand","kitkat","kittles"]; 
+
+var addElementToBeginningOfArray=(array, element)=>{ 
+var array1 = [element, ...array];
+} 
+
+var destructivelyAddElementToBeginningOfArray=(array,element)=>{ 
+array.unshift(element);
+} 
